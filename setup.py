ab7b381d7f60630029f666f436fccd071b550832
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-09-19 13:28:55 -0400

    Old school req parsing in setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,6 @@
 # -*- coding: utf-8 -*-
 import sys
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-try: # for pip >= 10
-    from pip._internal import download
-except ImportError: # for pip <= 9.0.3
-    from pip import download
-
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
@@ -20,10 +10,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = parse_requirements(
-    'requirements.txt', session=download.PipSession(),
-)
-pip_requirements = [str(r.req) for r in requirements]
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+pip_requirements = [r.strip() for r in requirements]
 
 test_requirements = [
     # TODO: put package test requirements here
