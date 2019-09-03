6f94419fdd6fa07e4806ef5724f78a1766458bb8
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-09-03 14:26:35 -0400

    Rework dependencies for release 0.12.2

--- setup.py
+++ setup.py
@@ -7,13 +7,15 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-pip_requirements = [r.strip() for r in requirements]
+install_requirements = [
+    'boto3>=1.7.42',
+    'botocore>=1.10.42',
+    'docutils>=0.14'
+]
 
 # Only install futures package if using a Python version <= 2.7
 if sys.version_info < (3, 0):
-    pip_requirements.append('futures==3.0.5')
+    install_requirements.append('futures==3.0.5')
 
 pkg_version = open("aws_lambda/_version.py").readlines()[-1].split()[-1].strip("\"'")
 
@@ -26,12 +28,13 @@ setup(
     version=pkg_version,
     description="FORKED for 4dn-dcic. Use to package and deploy lambda functions.",
     long_description=readme,
+    long_description_content_type='text/markdown',
     author="Carl Vitzthum, Soo Lee",
     author_email='carl_vitzthum@hms.harvard.edu',
     url='https://github.com/4dn-dcic/python-lambda',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=pip_requirements,
+    install_requires=install_requirements,
     license="ISCL",
     zip_safe=False,
     keywords='python-lambda',
