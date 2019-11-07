420c5302ee6acd47aa608f3935df8012531af502
William Ronchetti <wrr33@cornell.edu>
2019-11-07 14:08:51 -0500

    MIT license, fix setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,16 @@
 # -*- coding: utf-8 -*-
 import sys
-
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-install_requirements = [
-    'boto3>=1.10.7',
-    'botocore>=1.13.7',
-    'docutils>=0.14'
-]
-
-# Only install futures package if using a Python version <= 2.7
-if sys.version_info < (3, 0):
-    install_requirements.append('futures==3.0.5')
 
 pkg_version = open("aws_lambda/_version.py").readlines()[-1].split()[-1].strip("\"'")
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+with open('requirements.txt') as f:
+    install_requirements = f.read().splitlines()
+with open('dev-requirements.txt') as f:
+    test_requirements = f.read().splitlines()
 
-test_requirements = [
-    # TODO: put package test requirements here
-]
 
 setup(
     name='python-lambda-4dn',
@@ -28,8 +18,8 @@ setup(
     description="FORKED for 4dn-dcic. Use to package and deploy lambda functions.",
     long_description=readme,
     long_description_content_type='text/x-rst',
-    author="Carl Vitzthum, Soo Lee",
-    author_email='carl_vitzthum@hms.harvard.edu',
+    author="4DN Team at Harvard Medical School",
+    author_email='william_ronchetti@hms.harvard.edu',
     url='https://github.com/4dn-dcic/python-lambda',
     packages=find_packages(),
     include_package_data=True,
@@ -38,17 +28,11 @@ setup(
     zip_safe=False,
     keywords='python-lambda',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
