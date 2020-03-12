3516e26e590a17236531799d52cf560a56b4370f
Kent Pitman <kent_pitman@hms.harvard.edu>
2020-03-12 11:57:55 -0400

    Make aws_lambda/_version.py consistent with pyproject.toml.

--- aws_lambda/_version.py
+++ aws_lambda/_version.py
@@ -1,4 +1,6 @@
 """Version information."""
 
+# Information in this file is now superseded by the pyproject.toml file.
+
 # The following line *must* be the last in the module, exactly as formatted:
-__version__ = "1.0.2b1"
+__version__ = "2.0.0b1"
